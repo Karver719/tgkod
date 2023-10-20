70ae8d6099e329c583e4c7b38fc389bede873e76
Yaroslav <144015762+Katabym@users.noreply.github.com>
2023-10-20 21:20:12 +0300

    Add files via upload
    Add start kod

---
+++ main.py
@@ -0,0 +1,28 @@
+import telebot
+import sous
+
+token = "6363377954:AAGTtkjp4ov05F5hBG01HGk82LQZW739unw"
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def answer_to_start(message):
+    bot.send_message(message.chat.id, sous.startmess)
+
+@bot.message_handler(content_types=["text"])
+def answer_to_text(message):
+    global gorod_set
+    word = 'минск'
+    if word in message.text.lower():
+        bot.send_message(message.chat.id, 'Укажите ваш пол (мужской/женский)')
+        gorod_set = 1
+    elif message.text.lower() == 'мужской' and gorod_set == 1:
+        bot.send_message(message.chat.id, 'Йоу, у тебя получилось!')
+        # Если нужно изменить город и начать по новой добавьте 'gorod_set = 0'
+    elif message.text.lower() == 'женский' and gorod_set == 1:
+        bot.send_message(message.chat.id, 'Тоже нормально!')
+        # Здесь так же 'gorod_set = 0'
+    else:
+        bot.send_message(message.chat.id, "Извините, у вас другой город!")
+
+bot.polling(none_stop=True)
